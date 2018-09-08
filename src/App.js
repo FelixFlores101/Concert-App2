04aa170548f33775c16836e2db31f16a7b387316
Felix Flores <felflr101@gmail.com>
2018-09-07 21:56:49 -0400

    working on react

--- src/App.js
+++ src/App.js
@@ -4,13 +4,55 @@ import './App.css';
 import Item from './item.js'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      events: []
+    }
+  }
+
+  getBands() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:3000/events', true)
+    xhr.onload = function() {
+      let response = JSON.parse(xhr.responseText)
+      console.log(response.events)
+      this.setState({
+        events: response.events
+      })
+    }.bind(this)
+    xhr.send()
+
+
+  }
+
+  componentWillMount() {
+    this.getBands()
+  }
+
+  renderEvents() {
+    if(this.state.events.length > 0) {
+      this.state.events.map((event) => {
+        return (
+          <Item />
+        )
+      })
+    } else {
+      return (
+        <div>No Events Yet.</div>
+      )
+    }
+  }
+
   render() {
+    var renderEvents = this.state.events.map(function(event) {
+      return (
+        <Item key={event.id} event={event} />
+      )
+    })
     return (
       <div className="App container-grid">
-        <Item />
-        <Item />
-        <Item />
-        <Item />
+        {renderEvents}
       </div>
     );
   }
--- src/item.js
+++ src/item.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+
 
 
 export default class Item extends React.Component {
@@ -9,19 +10,45 @@ export default class Item extends React.Component {
     
         }
     }
+
+    handleEdit() {
+        
+    }
+
+    handleDelete() {
+        // this.deleteEvent()
+        var conf = window.confirm(`Are you sure you want to delete the event with the id of ${this.props.event.id}`)
+        if(!conf) {
+            // Do Nothing            
+        } else {
+            let xhr = new XMLHttpRequest()
+            xhr.open('delete', `http://localhost:3000/events/${this.props.event.id}`,true)
+            xhr.onload = () => {
+                console.log('Event Deleted')
+                alert('Event Deleted')
+            }
+            xhr.send()
+        }
+    }
+
+
+
     render() {
         return (
             <div className='item'>
-                <h5 className='item-title'>Name of Band</h5>
-                <i className='far fa-address-card"'></i>
+                <div className='item-title'>
+                    <h5>{this.props.event.band_name}</h5>
+                    <button onClick={this.handleEdit.bind(this)}>Edit</button>
+                    <button onClick={this.handleDelete.bind(this)}>Delete</button>
+                </div>
                 <div className='item-info'>
-                    <h6 className='muted'>Venue</h6>
-                    <h6 className='muted'>Date</h6>
+                    <h6 className='muted'>{this.props.event.place}</h6>
+                    <h6 className='muted'>{this.props.event.date}</h6>
                 </div>
                 <div className='comments'>
                     <h5 className='comment-title'>Add A Comment</h5>
                     <form>
-                        <textarea placeholder='Type your comment out' max='500'></textarea>
+                        <textarea placeholder='Type your comment out' maxLength='450'></textarea>
                     </form>
                 </div>
             </div>
